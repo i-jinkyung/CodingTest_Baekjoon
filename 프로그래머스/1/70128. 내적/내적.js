3ea31b6132bb5769f4ae1022799f31f788abdf77
i-jinkyung <110605460+i-jinkyung@users.noreply.github.com>
2024-02-23 12:31:05 +0900

    [level 1] Title: 내적, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+
+  let answer =[]
+  
+  for(let i=0; i<a.length; i++){
+    answer.push(a[i] * b[i])
+  }
+
+  return answer.reduce((acc, curr)=> {return acc + curr} )
+}
