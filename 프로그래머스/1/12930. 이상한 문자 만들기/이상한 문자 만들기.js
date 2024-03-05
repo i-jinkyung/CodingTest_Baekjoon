6ead9039832d892181741017e9276f5806af750b
i-jinkyung <110605460+i-jinkyung@users.noreply.github.com>
2024-03-05 12:21:42 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,10 +1,4 @@
 function solution(s) {
-  const newArr = s.split(" ").map((value, index) => {
-    return [...value]
-      .map((char, index) => {
-        return (index + 1) % 2 === 0 ? char.toLowerCase() : char.toUpperCase();
-      })
-      .join("");
-  });
-  return newArr.join(" ");
+  return s.split(" ").map((val, idx)=> {return [...val].map((char,idx)=> idx % 2 ===0 ? char.toUpperCase() : char.toLowerCase())}).join(' ').replaceAll(",",'')
+
 }
